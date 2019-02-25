05c79e86cd6ec2cdccda7be1928bbe77c247e1ee
Yasmin Abdirahman <yasminosman37@yahoo.com>
2019-02-25 13:01:08 +0300

    added loop for all questions

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("button#find").click(function quizMarks(event){
+    var marks = 0;
+    $("input:radio[name=definition1]:checked,input:radio[name=definition2]:checked,input:radio[name=definition3]:checked,input:radio[name=definition4]:checked").each(function() {
+      update = marks += parseInt($(this).val());
+      percentage = (update/50)*100;
+    });
+   document.getElementById("total").innerHTML = " YOUR SCORE IS: " + percentage + "%";
+ });
+});
