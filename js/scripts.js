0afcd49efe0c42e1ccae406446439c74f199bc7e
Yasmin Abdirahman <yasminosman37@yahoo.com>
2019-02-25 15:19:56 +0300

    proper syntax and indentation used

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 //Business logic
-$(document).ready(function(){
-  $("button#find").click(function quizMarks(event){
+$(document).ready(function() {
+  $("button#find").click(function quizMarks(event) {
     var marks = 0;
     $("input:radio[name=definition1]:checked,input:radio[name=definition2]:checked,input:radio[name=definition3]:checked,input:radio[name=definition4]:checked").each(function() {
       update = marks += parseInt($(this).val());
-      percentage = (update/50)*100;
+      percentage = (update / 50) * 100;
     });
 
 
@@ -13,7 +13,7 @@ $(document).ready(function(){
 
 
     //Output to User interface
-   document.getElementById("total").innerHTML = " YOUR SCORE IS: " + percentage + "%";
+    document.getElementById("total").innerHTML = " YOUR SCORE IS: " + percentage + "%";
 
- });
+  });
 });
